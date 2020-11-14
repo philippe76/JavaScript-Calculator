bbea0f4d06d4c494b1743b599013b7e45954e54a
Philippe Lanougadere <soufiane92230@gmail.com>
2020-11-14 22:53:55 +0100

    Display operators : Step 1

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 
-const input_numbers = document.querySelectorAll('.calc div');
-const display = document.querySelector('#display div');
+const numbers = document.querySelectorAll('.calc div');
+const operators = document.querySelectorAll('.calc button');
 
-input_numbers.forEach(item => {
+const display = document.querySelector('#display div');
+const memo = document.querySelector('#display-memo div');
+
+
+// display input numbers
+numbers.forEach(item => {
     item.addEventListener('click', () => {
-        console.log(display.innerHTML);
         display.innerHTML += item.innerHTML; 
         display.innerHTML = +display.innerHTML
+        memo.innerHTML = display.innerHTML; 
     })
 })
+
+// clear input
+document.getElementById('clear').addEventListener('click', () => {
+    display.innerHTML = 0;
+    memo.innerHTML = 0;
+})
+
+// display input operators
+operators.forEach(item => {
+    item.addEventListener('click', () => {
+        if(item != clear && item != equals){
+            display.innerHTML = 0;
+            memo.innerHTML += item.innerHTML
+        }
+    })    
+})
+
+
+
+// when hitting 'equals'
+const calculate = ([...args], [...operators]) => {
+    // console.log([...args] + ' ' + [...operators]);
+    if ([...operators][0] === '+'){
+        return [...args][0] + [...args][1]
+    }
+}
+
