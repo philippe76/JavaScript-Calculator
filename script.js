555f3019db55aca3ea5d8eb8ff0e87bdf88418b5
Philippe Lanougadere <soufiane92230@gmail.com>
2020-11-14 23:46:27 +0100

    Display operators: Step 2

--- script.js
+++ script.js
@@ -6,29 +6,50 @@ const display = document.querySelector('#display div');
 const memo = document.querySelector('#display-memo div');
 
 
+// function to check if used operator  
+// const isOperator = str => {
+//     operators.forEach(item => {
+//         console.log(str.includes(item.innerHTML));
+//         return str.includes(item.innerHTML)
+//     })
+// }
+
+
+
 // display input numbers
 numbers.forEach(item => {
-    item.addEventListener('click', () => {
-        display.innerHTML += item.innerHTML; 
-        display.innerHTML = +display.innerHTML
-        memo.innerHTML = display.innerHTML; 
+    item.addEventListener('click', (e) => {
+
+        if (isNaN(display.innerHTML) && memo.innerHTML != '0' ){
+            display.innerHTML = item.innerHTML; 
+            display.innerHTML = display.innerHTML
+            memo.innerHTML += display.innerHTML; 
+        }
+        else {
+            display.innerHTML += item.innerHTML; 
+            display.innerHTML = +display.innerHTML
+            memo.innerHTML = display.innerHTML; 
+        }
+
     })
 })
 
-// clear input
-document.getElementById('clear').addEventListener('click', () => {
-    display.innerHTML = 0;
-    memo.innerHTML = 0;
-})
 
 // display input operators
 operators.forEach(item => {
     item.addEventListener('click', () => {
         if(item != clear && item != equals){
-            display.innerHTML = 0;
+            display.innerHTML = item.innerHTML;
             memo.innerHTML += item.innerHTML
-        }
+        }        
     })    
+})
+
+
+// clear input
+document.getElementById('clear').addEventListener('click', () => {
+    display.innerHTML = 0;
+    memo.innerHTML = 0;
 })
 
 
